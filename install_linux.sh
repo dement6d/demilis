4c9ee8f1d0acabf7d852e69b7648a414a9142bc4
dement6d <cuff.ho@gmail.com>
2022-02-02 18:19:47 +0100

    added linux installation script

---
+++ install_linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as sudo, \"sudo ./install_linux.sh\""
+  exit
+fi
+dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained true
+
+mv bin/Release/net6.0/linux-x64/publish/demilis /usr/bin/
+chmod +x /usr/bin/demilis
+
+echo ""
+echo "Demilis installed in /usr/bin/demilis"
