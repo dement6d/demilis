95b448f2948e75872420c9527784cdd6feeae354
dement6d <93228501+dement6d@users.noreply.github.com>
2022-02-04 04:21:06 +0100

    geo testing 3

--- Command/Commands/List.cs
+++ Command/Commands/List.cs
@@ -61,56 +61,13 @@ namespace demilis.Command.Commands
         {
             try
             {
-                IPLocation IPLocation;
-                string retJson = DownloadDataNoAuth(string.Format("https://www.freegeoip.net/json/{0}", ip));
-                var JO = JObject.Parse(retJson);
-                return (string)JO["country_name"];
+                IpInfoApi ipInfoApi = new IpInfoApi(new HttpClient());
+                return ipInfoApi.GetLocationByIpAsync(ip).Result;
             }
             catch
             {
                 return "Unknown";
             }
         }
-        public string DownloadDataNoAuth(string hostURI)
-        {
-            string retXml = string.Empty;
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(hostURI);
-                request.Method = "GET";
-                String responseLine = String.Empty;
-                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-                {
-                    using (Stream dataStream = response.GetResponseStream())
-                    {
-                        StreamReader sr = new StreamReader(dataStream);
-                        retXml = sr.ReadToEnd();
-                        sr.Close();
-                        dataStream.Close();
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                retXml = null;
-            }
-            return retXml;
-        }
-    }
-    public class IPLocation
-    {
-        public string IPAddress { get; set; }
-        public string CountryCode { get; set; }
-        public string CountryName { get; set; }
-        public string RegionCode { get; set; }
-        public string Region { get; set; }
-        public string CityCode { get; set; }
-        public string CityName { get; set; }
-        public string ISP { get; set; }
-        public string Organization { get; set; }
-        public string Latitude { get; set; }
-        public string Longitude { get; set; }
-        public string ZipCode { get; set; }
-        public string TimeZone { get; set; }
     }
 }
