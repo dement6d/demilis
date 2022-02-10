66efcecc3c3020aaea675c4ec8ddf59e6577b417
dement6d <93228501+dement6d@users.noreply.github.com>
2022-02-10 17:20:29 +0100

    separate source from install scripts

--- install_linux.sh
+++ install_linux.sh
@@ -4,9 +4,10 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as sudo, \"sudo ./install_linux.sh\""
   exit
 fi
+cd src
 dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained true
 
-mv bin/Release/net6.0/linux-x64/publish/demilis /usr/bin/
+mv src/bin/Release/net6.0/linux-x64/publish/demilis /usr/bin/
 chmod +x /usr/bin/demilis
 
 echo ""
