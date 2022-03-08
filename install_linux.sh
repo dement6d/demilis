9efa5988a5f53daffddf357888c676359cad7c05
dement6d <cuff.ho@gmail.com>
2022-03-08 20:52:55 +0100

    update install linux script

--- install_linux.sh
+++ install_linux.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as sudo, \"sudo ./install_linux.sh\""
-  exit
-fi
 cd src
 dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained true
 
-mv bin/Release/net6.0/linux-x64/publish/demilis /usr/bin/
-chmod +x /usr/bin/demilis
+sudo mv bin/Release/net6.0/linux-x64/publish/demilis /usr/bin/
+sudo chmod +x /usr/bin/demilis
 
 echo ""
 echo "Demilis installed in /usr/bin/demilis"
