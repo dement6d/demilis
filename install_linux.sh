d1b30b5c8ae2c13eb7c6a512c841b40f37935a95
dement6d <cuff.ho@gmail.com>
2022-05-01 03:11:59 +0200

    update install script and csproj

--- install_linux.sh
+++ install_linux.sh
@@ -6,5 +6,7 @@ dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-containe
 sudo mv bin/Release/net6.0/linux-x64/publish/demilis /usr/bin/
 sudo chmod +x /usr/bin/demilis
 
+sudo bash -c "echo $'[Desktop Entry]\nName=demilis\nGenericName=Multiple connection TCP listener\nExec=/usr/bin/demilis -p 8080\nTerminal=true\nType=Application\nCategories=Utility;\nIcon=demilis\nPath=/usr/bin' > /usr/share/applications/demilis.desktop"
+
 echo ""
 echo "Demilis installed in /usr/bin/demilis"
